91e8c9d1bccc5d199378bf72b4db710480cdef76
Seventy Greene <seventygreen@gmail.com>
2025-01-16 22:29:38 -0800

    epoll

--- multi-io.c
+++ multi-io.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/poll.h>
+#include <sys/epoll.h>
 
 int main() {
 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -15,38 +15,36 @@ int main() {
 	bind(sockfd, (struct sockaddr *)&serveraddr, sizeof(struct sockaddr));
 	listen(sockfd, 10);
 
-	struct pollfd fds[1024] = {0};
-	fds[sockfd].fd = sockfd;
-	fds[sockfd].events = POLLIN;
-	int maxfd = sockfd;
-
+	int epfd = epoll_create(1);  // 4
+	struct epoll_event ev;
+	ev.data.fd = sockfd;
+	ev.events = EPOLLIN;
+	epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &ev);
+	struct epoll_event events[1024] = {0};
 	while (1) {
-		poll(fds, maxfd + 1, -1);
-		if (fds[sockfd].revents & POLLIN) {
-			struct sockaddr_in clientaddr;
-			socklen_t len;
-			int clientfd = accept(sockfd, (struct sockaddr*)&clientaddr, &len);
-			printf("Accepted: %d\n", clientfd);
-
-			fds[clientfd].fd = clientfd;
-			fds[clientfd].events = POLLIN;
-			maxfd = clientfd;
-		} 
-
-		for (int i = sockfd + 1; i <= maxfd; i++) {
-			if (fds[i].revents & POLLIN) {
+		int nready = epoll_wait(epfd, events, 1024, -1);
+		for (int i = 0; i < nready; i++) {
+			int connfd = events[i].data.fd;
+			if (sockfd == connfd) {
+				struct sockaddr_in clientaddr;
+				socklen_t len;
+				int clientfd = accept(sockfd, (struct sockaddr*)&clientaddr, &len);
+				ev.data.fd = clientfd;
+				ev.events = EPOLLIN | EPOLLET;
+				epoll_ctl(epfd, EPOLL_CTL_ADD, clientfd, &ev);
+				printf("Accepted: %d\n", clientfd);
+			} else if (events[i].events & EPOLLIN) {
 				char buffer[128] = {0};
-				int count = recv(i, buffer, 128, 0);
+				int count = recv(connfd, buffer, 128, 0);
 				if (count == 0) {
-					printf("Disconnected %d\n", i);
-					fds[i].fd = -1;
-					fds[i].events = 0;
+					printf("Disconnected %d\n", connfd);
+					epoll_ctl(epfd, EPOLL_CTL_DEL, connfd, NULL);		
 					close(i);
 					continue;
 				}
 				
-				send(i, buffer, count, 0);
-				printf("clientfd: %d, count: %d, buffer: %s\n", i, count, buffer);
+				send(connfd, buffer, count, 0);
+				printf("clientfd: %d, count: %d, buffer: %s\n", connfd, count, buffer);
 			}
 		}
 	}
